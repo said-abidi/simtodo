4ce5c8e554f8d710cbd702747dc40d9283b4c432
Romain Endelin <rendelin@zendesk.com>
2017-12-20 15:11:03 +0100

    Add main to Simtodo + __init__ file

--- simtodo/simtodo.py
+++ simtodo/simtodo.py
@@ -8,29 +8,34 @@ import logger
 import display
 import action
 
-# workflow bloc in alfred
-BLOC = int(sys.argv[1])
-logger.log("Bloc '" + str(BLOC) + "'")
 
-# argument received
-ARG = sys.argv[2]
-logger.log("argument received='" + ARG + "'")
+def main():
+    # workflow bloc in alfred
+    BLOC = int(sys.argv[1])
+    logger.log("Bloc '" + str(BLOC) + "'")
 
-if BLOC == 1:
-    # first bloc is used to display tasklist or add new task
-    display.display(ARG)
+    # argument received
+    ARG = sys.argv[2]
+    logger.log("argument received='" + ARG + "'")
 
-if BLOC == 2:
-    # second bloc is used to display task menu
-    if ARG.startswith(action.ADD_TASK + action.SEPARATOR):
-        # ignore if it starts with an action
-        display.display_nothing()
-    else:
-        display.display_menu(ARG)
+    if BLOC == 1:
+        # first bloc is used to display tasklist or add new task
+        display.display(ARG)
 
-if BLOC == 3:
-    # third bloc is used to perform action
-    ACTION, QUERY = parser.parse_argument(ARG)
-    logger.log("action='" + ACTION + "'")
-    logger.log("query='" + QUERY + "'")
-    action.PERFORM_ACTION[ACTION](QUERY)
+    if BLOC == 2:
+        # second bloc is used to display task menu
+        if ARG.startswith(action.ADD_TASK + action.SEPARATOR):
+            # ignore if it starts with an action
+            display.display_nothing()
+        else:
+            display.display_menu(ARG)
+
+    if BLOC == 3:
+        # third bloc is used to perform action
+        ACTION, QUERY = parser.parse_argument(ARG)
+        logger.log("action='" + ACTION + "'")
+        logger.log("query='" + QUERY + "'")
+        action.PERFORM_ACTION[ACTION](QUERY)
+
+if __name__ == '__main__':
+    main()
